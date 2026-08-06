15cfa69bda545104c0273b7c457edb4f7a96e27c
Muru24 <162981848+Muru24@users.noreply.github.com>
2026-08-07 00:36:26 +0900

    BT 공통 구조 수정
    DataAssets으로 계속 추적형, 1회 추적형, 대기형 등등으로 수정 가능하게 바꿈

--- TeamProject_MOU/Source/TeamProject_MOU/Public/Enum/NPCEnum.h
+++ TeamProject_MOU/Source/TeamProject_MOU/Public/Enum/NPCEnum.h
@@ -16,3 +16,37 @@ enum class ENpcType : uint8
 	Hold		UMETA(DisplayName = "Hold", ToolTip = "플레이어를 드는 NPC"),
 	Blocking	UMETA(DisplayName = "Blocking", ToolTip = "플레이어의 길을 막는 NPC")
 };
+
+/* NPC 시작 상태*/
+UENUM(BlueprintType)
+enum class ENPCStartState : uint8
+{
+    Stay,
+    Patrol
+};
+
+/*NPC 행동 후 정책*/
+UENUM(BlueprintType)
+enum class ENPCAfterActionPolicy : uint8
+{
+    /*타깃이 계속 보이면 다시 추적/공격 반복*/
+    RepeatWhileTargetVisible,
+    /*행동 후 정지*/
+    OneShotThenStay,
+    /*행동 후 추적*/
+    OneShotThenTracking,
+    /*행동 후 정찰*/
+    OneShotThenPatrol
+};
+
+/*NPC 타겟을 잃었을 때 정책*/
+UENUM(BlueprintType)
+enum class ENPCLostTargetPolicy : uint8
+{
+    /*행동 후 대기*/
+    ReturnToStay,
+    /*행동 후 정찰*/
+    ReturnToPatrol,
+    /*행동 후 원래 위치로*/
+    ReturnHome
+};
