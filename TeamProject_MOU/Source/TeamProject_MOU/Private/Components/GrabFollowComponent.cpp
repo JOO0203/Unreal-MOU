23e610a182be3d582a0f85a6693dae677b822095
Muru24 <162981848+Muru24@users.noreply.github.com>
2026-08-09 19:20:09 +0900

    npc 작업중

---
+++ TeamProject_MOU/Source/TeamProject_MOU/Private/Components/GrabFollowComponent.cpp
@@ -0,0 +1,118 @@
+﻿#include "Components/GrabFollowComponent.h"
+
+#include "Components/StatusComponent.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Net/UnrealNetwork.h"
+
+UGrabFollowComponent::UGrabFollowComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+	SetIsReplicatedByDefault(true);
+}
+
+void UGrabFollowComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void UGrabFollowComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (CarrierCharacter)
+	{
+		SyncGrabTransform();
+	}
+}
+
+void UGrabFollowComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(UGrabFollowComponent, CarrierCharacter);
+	DOREPLIFETIME(UGrabFollowComponent, GrabSocketName);
+	DOREPLIFETIME(UGrabFollowComponent, RelativeOffset);
+	DOREPLIFETIME(UGrabFollowComponent, bFollowRotation);
+}
+
+void UGrabFollowComponent::StartGrabFollow(ACharacter* NewCarrier, FName NewSocketName, FVector NewRelativeOffset, bool bInheritRotation)
+{
+	CarrierCharacter = NewCarrier;
+	GrabSocketName = NewSocketName;
+	RelativeOffset = NewRelativeOffset;
+	bFollowRotation = bInheritRotation;
+
+	ApplyHeldTag(true);
+	SyncGrabTransform();
+}
+
+void UGrabFollowComponent::StopGrabFollow()
+{
+	CarrierCharacter = nullptr;
+	ApplyHeldTag(false);
+}
+
+void UGrabFollowComponent::OnRep_GrabState()
+{
+	if (CarrierCharacter)
+	{
+		SyncGrabTransform();
+	}
+}
+
+void UGrabFollowComponent::SyncGrabTransform()
+{
+	ACharacter* OwnerCharacter = Cast<ACharacter>(GetOwner());
+	if (!OwnerCharacter || !CarrierCharacter)
+	{
+		return;
+	}
+
+	USkeletalMeshComponent* CarrierMesh = CarrierCharacter->GetMesh();
+	FTransform TargetTransform = CarrierCharacter->GetActorTransform();
+
+	if (CarrierMesh && CarrierMesh->DoesSocketExist(GrabSocketName))
+	{
+		TargetTransform = CarrierMesh->GetSocketTransform(GrabSocketName, RTS_World);
+	}
+
+	const FVector TargetLocation = TargetTransform.TransformPosition(RelativeOffset);
+	const FRotator TargetRotation = bFollowRotation ? TargetTransform.GetRotation().Rotator() : OwnerCharacter->GetActorRotation();
+
+	OwnerCharacter->SetActorLocationAndRotation(TargetLocation, TargetRotation, false, nullptr, ETeleportType::TeleportPhysics);
+
+	if (UCharacterMovementComponent* MovementComponent = OwnerCharacter->GetCharacterMovement())
+	{
+		MovementComponent->StopMovementImmediately();
+	}
+}
+
+void UGrabFollowComponent::ApplyHeldTag(bool bAdd) const
+{
+	if (!GetOwner() || !GetOwner()->HasAuthority())
+	{
+		return;
+	}
+
+	const FGameplayTag HeldTag = FGameplayTag::RequestGameplayTag(FName("State.Held"), false);
+	if (!HeldTag.IsValid())
+	{
+		return;
+	}
+
+	if (ACharacter* OwnerCharacter = Cast<ACharacter>(GetOwner()))
+	{
+		if (UStatusComponent* StatusComponent = OwnerCharacter->FindComponentByClass<UStatusComponent>())
+		{
+			if (bAdd)
+			{
+				StatusComponent->AddStatusTag(HeldTag);
+			}
+			else
+			{
+				StatusComponent->RemoveStatusTag(HeldTag);
+			}
+		}
+	}
+}
---
+++ TeamProject_MOU/Source/TeamProject_MOU/Public/Components/GrabFollowComponent.h
@@ -0,0 +1,63 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "GrabFollowComponent.generated.h"
+
+class ACharacter;
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class TEAMPROJECT_MOU_API UGrabFollowComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UGrabFollowComponent();
+
+	virtual void BeginPlay() override;
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	/*그랩 시작*/
+	UFUNCTION(BlueprintCallable, Category = "Grab")
+	void StartGrabFollow(ACharacter* NewCarrier, FName NewSocketName = TEXT("GrabSocket"), FVector NewRelativeOffset = FVector::ZeroVector, bool bInheritRotation = true);
+
+	/*그랩 정지*/
+	UFUNCTION(BlueprintCallable, Category = "Grab")
+	void StopGrabFollow();
+
+	/*땅 상태 체크*/
+	UFUNCTION(BlueprintPure, Category = "Grab")
+	bool IsGrabbed() const { return CarrierCharacter != nullptr; }
+
+	/*그랩당할 캐릭터 확인*/
+	UFUNCTION(BlueprintPure, Category = "Grab")
+	ACharacter* GetCarrierCharacter() const { return CarrierCharacter; }
+
+protected:
+	/*그랩 상태 캐릭터*/
+	UPROPERTY(ReplicatedUsing = OnRep_GrabState, VisibleAnywhere, BlueprintReadOnly, Category = "Grab")
+	TObjectPtr<ACharacter> CarrierCharacter;
+
+	/*그랩 소켓 이름*/
+	UPROPERTY(Replicated, VisibleAnywhere, BlueprintReadOnly, Category = "Grab")
+	FName GrabSocketName = TEXT("GrabSocket");
+
+	/*그랩 상대 위치*/
+	UPROPERTY(Replicated, VisibleAnywhere, BlueprintReadOnly, Category = "Grab")
+	FVector RelativeOffset = FVector::ZeroVector;
+
+	/*그랩 회전 여부*/
+	UPROPERTY(Replicated, VisibleAnywhere, BlueprintReadOnly, Category = "Grab")
+	bool bFollowRotation = true;
+
+	/*그랩 상태 변경 시 호출되는 함수*/
+	UFUNCTION()
+	void OnRep_GrabState();
+
+private:
+	/*그랩 위치 동기화*/
+	void SyncGrabTransform();
+	/*그랩 상태 태그 적용*/
+	void ApplyHeldTag(bool bAdd) const;
+};
